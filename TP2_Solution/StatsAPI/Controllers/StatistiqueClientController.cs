f0bbcb46d291a4aaf1cebe5c637c809f7d79d9a6
Antoine <AntoineRioux.AR@gmail.com>
2023-04-27 14:05:14 -0400

    Controllers de StatsAPI prêt

--- TP2_Solution/StatsAPI/Controllers/StatistiqueClientController.cs
+++ TP2_Solution/StatsAPI/Controllers/StatistiqueClientController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -9,36 +11,111 @@ namespace StatsAPI.Controllers
     [ApiController]
     public class StatistiqueClientController : ControllerBase
     {
-        // GET: api/<StatistiqueClientController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private StatsDbContext _context = new StatsDbContext();
+        private HttpClient _httpClient;
+        private JsonSerializerOptions _serializerOptions;
+
+        public StatistiqueClientController()
         {
-            return new string[] { "value1", "value2" };
+            _httpClient = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
         }
 
-        // GET api/<StatistiqueClientController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        ///
+        /// <summary> Retourne les stats d'un utilisateur selon l'id spécifié</summary>
+        /// 
+
+        // GET: api/<StatistiqueClientController>/id
+        [HttpGet("{UtilisateurId}", Name = "GetStatsClient")]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public IActionResult GetStatsClient(Guid UtilisateurId)
         {
-            return "value";
+            try
+            {
+                Models.StatistiqueClient statsClient = _context.StatistiqueClients.Find(UtilisateurId);
+                if (statsClient != null)
+                {
+                    return Ok(statsClient);
+                }
+                else
+                {
+                    return StatusCode((int)HttpStatusCode.NotFound);
+                }
+            }
+            catch (Exception)
+            {
+            }
+            return BadRequest();
         }
+
+
+        ///
+        /// <summary> Quand un nouvel utilisateur est ajouté, on l'ajoute aussi dans la BD de stats</summary>
+        /// 
 
         // POST api/<StatistiqueClientController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        [ProducesResponseType(typeof(Models.StatistiqueClient), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        public IActionResult PostNewClientStats([FromBody] Models.StatistiqueClient model)
         {
+            try
+            {
+                Models.StatistiqueClient client = _context.StatistiqueClients.Find(model.UtilisateurId);
+                if (client == null) //on peut seulement ajouter s'il n'existe pas déjà
+                {
+                    Models.StatistiqueClient statsClient = new Models.StatistiqueClient()
+                    {
+                        UtilisateurId = model.UtilisateurId,
+                        TotalArticleBought = 0,
+                        TotalCashSpent = 0
+                    };
+                    _context.Add(statsClient);
+                    _context.SaveChanges();
+                    return CreatedAtAction(nameof(GetStatsClient), new { UtilisateurId = statsClient.UtilisateurId }, statsClient);
+                }
+            }
+            catch (Exception)
+            {
+            }
+            return BadRequest();
         }
 
-        // PUT api/<StatistiqueClientController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        ///
+        /// <summary> Pour mettre à jour les stats d'un utilisateur </summary>
+        /// 
 
-        // DELETE api/<StatistiqueClientController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // PUT api/<StatistiqueVendeurController>/5
+        [HttpPut("{UtilisateurId}")]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public IActionResult EditStatsClient(Guid UtilisateurId, [FromBody] Models.StatistiqueClient model)
         {
+            try
+            {
+                Models.StatistiqueClient statsClient = _context.StatistiqueClients.Find(UtilisateurId);
+                if (statsClient != null)
+                {
+                    statsClient.TotalArticleBought = model.TotalArticleBought;
+                    statsClient.TotalCashSpent = model.TotalCashSpent;
+                    _context.SaveChanges();
+                    return Ok();
+                }
+                else
+                {
+                    return StatusCode((int)HttpStatusCode.NotFound);
+                }
+            }
+            catch (Exception)
+            {
+            }
+            return BadRequest();
         }
     }
 }
--- TP2_Solution/StatsAPI/Controllers/StatistiqueVendeurController.cs
+++ TP2_Solution/StatsAPI/Controllers/StatistiqueVendeurController.cs
@@ -21,7 +21,12 @@ namespace StatsAPI.Controllers
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase
             };
         }
-        // GET: api/<StatistiqueVendeurController>
+
+        ///
+        /// <summary> Retourne les stats d'un utilisateur selon l'id spécifié</summary>
+        /// 
+
+        // GET: api/<StatistiqueVendeurController>/id
         [HttpGet("{UtilisateurId}", Name = "GetStatsVendeur")]
         [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
@@ -46,29 +51,72 @@ namespace StatsAPI.Controllers
             return BadRequest();
         }
 
-        // GET api/<StatistiqueVendeurController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+
+        ///
+        /// <summary> Quand un nouvel utilisateur est ajouté, on l'ajoute aussi dans la BD de stats</summary>
+        /// 
 
         // POST api/<StatistiqueVendeurController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        [ProducesResponseType(typeof(Models.StatistiqueVendeur), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        public IActionResult PostNewVendeurStats([FromBody] Models.StatistiqueVendeur model)
         {
+            try
+            {
+                Models.StatistiqueVendeur vendeur = _context.StatistiqueVendeurs.Find(model.UtilisateurId);
+                if (vendeur == null) //on peut seulement ajouter s'il n'existe pas déjà
+                {
+                    Models.StatistiqueVendeur statsVendeur = new Models.StatistiqueVendeur()
+                    {
+                        UtilisateurId = model.UtilisateurId,
+                        TotalCashReceved = 0,
+                        Profit = 0,
+                        TotalArticleSold = 0
+                    };
+                    _context.Add(statsVendeur);
+                    _context.SaveChanges();
+                    return CreatedAtAction(nameof(GetStatsVendeur), new { UtilisateurId = statsVendeur.UtilisateurId }, statsVendeur);
+                }
+            }
+            catch (Exception)
+            {
+            }
+            return BadRequest();
         }
+
+        ///
+        /// <summary> Pour mettre à jour les stats d'un utilisateur </summary>
+        /// 
 
         // PUT api/<StatistiqueVendeurController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut("{UtilisateurId}")]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public IActionResult EditStatsVendeur(Guid UtilisateurId, [FromBody] Models.StatistiqueVendeur model)
         {
+            try
+            {
+                Models.StatistiqueVendeur statsVendeur = _context.StatistiqueVendeurs.Find(UtilisateurId);
+                if (statsVendeur != null)
+                {
+                    statsVendeur.TotalCashReceved = model.TotalCashReceved;
+                    statsVendeur.Profit = model.Profit;
+                    statsVendeur.TotalArticleSold = model.TotalArticleSold;
+                    _context.SaveChanges();
+                    return Ok();
+                }
+                else
+                {
+                    return StatusCode((int)HttpStatusCode.NotFound);
+                }
+            }
+            catch (Exception)
+            {
+            }
+            return BadRequest();
         }
 
-        // DELETE api/<StatistiqueVendeurController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
